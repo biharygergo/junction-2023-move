9b2f626bd5b73b5dc0857a214ecacb540ae272a7
Gergely Bihary <biharygergo@gmail.com>
2023-11-11 01:02:46 +0200

    feat: Working segmentation model

--- src/GameScreen/Segmentation.tsx
+++ src/GameScreen/Segmentation.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import "./Segmentation.css";
 
 import "@tensorflow/tfjs-core";
@@ -6,13 +6,7 @@ import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl";
 import * as bodySegmentation from "@tensorflow-models/body-segmentation";
 import "@mediapipe/selfie_segmentation";
-import { SelfieSegmentationConfig } from "@mediapipe/selfie_segmentation";
 
-// Uncomment the line below if you want to use TensorFlow.js runtime.
-/* // import '@tensorflow/tfjs-converter';
-
-// Uncomment the line below if you want to use MediaPipe runtime.
- */
 
 const model = bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation; // or 'BodyPix'
 const segmenterConfig = {
@@ -29,23 +23,20 @@ export function Segmentation() {
   useEffect(() => {
     // Not showing vendor prefixes.
     (window.navigator as any).getUserMedia(
-      { video: true, audio: false,  },
+      { video: true, audio: false },
       function (localMediaStream: any) {
         const video = document.querySelector("video") as HTMLVideoElement;
         video.srcObject = localMediaStream;
 
-        // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
-        // See crbug.com/110938.
-        video.onloadedmetadata = function (e: any) {
-          // Ready to go. Do some stuff.
-        };
-
-        console.log('creating segmenter')
+        console.log("Creating segmenter...");
         bodySegmentation
           .createSegmenter(model, segmenterConfig as any)
           .then(async (segmenter) => {
             segmenterRef.current = segmenter;
-            animationLoop();
+
+            video.addEventListener("loadedmetadata", () => {
+              animationLoop();
+            });
           });
       },
       () => {}
@@ -53,50 +44,39 @@ export function Segmentation() {
   }, []);
 
   const animationLoop = () => {
-    renderSegmentation();
-    requestAnimationFrame(animationLoop);
-  }
+    try {
+      renderSegmentation();
+      requestAnimationFrame(animationLoop);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   const renderSegmentation = async () => {
-    const segmenter = segmenterRef.current;
+    const segmenter = segmenterRef.current as bodySegmentation.BodySegmenter;
     const video = document.getElementById("video") as HTMLVideoElement;
-    let people = await segmenter.segmentPeople(video as HTMLVideoElement);
+    const people = await segmenter.segmentPeople(video as HTMLVideoElement);
 
-    const foregroundColor = { r: 0, g: 0, b: 0, a: 0 };
-    const backgroundColor = { r: 0, g: 0, b: 0, a: 255 };
-    const drawContour = true;
-    const foregroundThreshold = 0.6;
-
-    const backgroundDarkeningMask = await bodySegmentation.toBinaryMask(
-      people,
-      foregroundColor,
-      backgroundColor,
-      drawContour,
-      foregroundThreshold
-    );
-
-    const opacity = 1;
-    const maskBlurAmount = 3; // Number of pixels to blur by.
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-/* 
-    const context = canvas.getContext('2d');
-    context?.clearRect(0, 0, 350, 700);
-    canvas.getContext('2d')?.putImageData(backgroundDarkeningMask, 0, 0);
-    console.log("got people", people);
-    console.log("drawing mask..."); */
-    await bodySegmentation.drawMask(
-      canvas,
-      video,
-      backgroundDarkeningMask,
-      opacity,
-      maskBlurAmount
-    );
+
+    const ctx = canvas.getContext("2d");
+
+    ctx?.clearRect(0, 0, 640, 480); // clear the image first
+
+    ctx?.drawImage(await people?.[0]?.mask?.toCanvasImageSource(), 0, 0);
+
+    // Add the original video back in (in image) , but only overwrite overlapping pixels.
+    if (ctx && video) {
+      ctx.globalCompositeOperation = "source-in";
+      ctx.drawImage(video, 0, 0, 640, 480);
+      ctx.globalCompositeOperation = "source-over";
+    }
   };
 
   return (
     <>
       <video id="video" autoPlay={true}></video>
-      <canvas id="canvas"></canvas>
+      <canvas id="canvas" width={640} height={480}></canvas>
     </>
   );
 }
