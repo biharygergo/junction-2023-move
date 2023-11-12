af44d0bf9c490b415d6eee283bbb74a166114a17
breacon <elodimarci@gmail.com>
2023-11-12 05:59:25 +0200

    built the whole thing alone

--- src/levels/level-4.ts
+++ src/levels/level-4.ts
@@ -15,109 +15,149 @@ export const level4 = {
             "param": 3
         },
         {
-            "startTime": 3.97,
+            "startTime": 1.97,
             "type": "jump",
-            "param": 3
-        },
-        {
-            "startTime": 5.67,
-            "type": "box",
-            "param": 3
-        },
-        {
-            "startTime": 7.64,
-            "type": "box",
-            "param": 3
-        },
-        {
-            "startTime": 9.8,
-            "type": "jump",
-            "param": 2
-        },
-        {
-            "startTime": 11.79,
-            "type": "box",
-            "param": 3
-        },
-        {
-            "startTime": 13.33,
-            "type": "jump",
-            "param": 3
-        },
-        {
-            "startTime": 15.64,
-            "type": "box",
-            "param": 3
-        },
-        {
-            "startTime": 17.19,
-            "type": "box",
-            "param": 4
-        },
-        {
-            "startTime": 19.44,
-            "type": "box",
-            "param": 2
-        },
-        {
-            "startTime": 21.73,
-            "type": "spin",
-            "param": 2
-        },
-        {
-            "startTime": 23.64,
-            "type": "box",
-            "param": 3
-        },
-        {
-            "startTime": 25.15,
-            "type": "box",
-            "param": 4
-        },
-        {
-            "startTime": 26.89,
-            "type": "box",
-            "param": 2
-        },
-        {
-            "startTime": 28.67,
-            "type": "box",
-            "param": 3
-        },
-        {
-            "startTime": 30.53,
-            "type": "box",
-            "param": 3
-        },
-        {
-            "startTime": 32.82,
-            "type": "box",
-            "param": 3
-        },
-        {
-            "startTime": 34.8,
-            "type": "box",
-            "param": 4
-        },
-        {
-            "startTime": 36.79,
-            "type": "spin",
             "param": 1
         },
         {
-            "startTime": 38.61,
-            "type": "spin",
+            "startTime": 3.94,
+            "type": "box",
+            "param": 1
+        },
+        {
+            "startTime": 4.5,
+            "type": "box",
+            "param": 3
+        },
+        {
+            "startTime": 6.47,
+            "type": "jump",
             "param": 2
         },
         {
-            "startTime": 40.96,
-            "type": "spin",
+            "startTime": 8.44,
+            "type": "box",
+            "param": 3
+        },
+        {
+            "startTime": 10.41,
+            "type": "box",
+            "param": 1
+        },
+        {
+            "startTime": 12.38,
+            "type": "box",
             "param": 2
         },
         {
-            "startTime": 43.02,
+            "startTime": 14.35,
             "type": "box",
             "param": 4
+        },
+        {
+            "startTime": 16.32,
+            "type": "box",
+            "param": 4
+        },
+        {
+            "startTime": 18.29,
+            "type": "box",
+            "param": 2
+        },
+        {
+            "startTime": 20.26,
+            "type": "spin",
+            "param": 3
+        },
+        {
+            "startTime": 22.23,
+            "type": "box",
+            "param": 4
+        },
+        {
+            "startTime": 24.2,
+            "type": "box",
+            "param": 2
+        },
+        {
+            "startTime": 26.17,
+            "type": "jump",
+            "param": 1
+        },
+        {
+            "startTime": 28.14,
+            "type": "jump",
+            "param": 1
+        },
+        {
+            "startTime": 28.14,
+            "type": "box",
+            "param": 3
+        },
+        {
+            "startTime": 28.14,
+            "type": "box",
+            "param": 4
+        },
+        {
+            "startTime": 30.11,
+            "type": "jump",
+            "param": 1
+        },
+        {
+            "startTime": 32.08,
+            "type": "spin",
+            "param": 2
+        },
+        {
+            "startTime": 34.05,
+            "type": "jump",
+            "param": 2
+        },
+        {
+            "startTime": 36.02,
+            "type": "spin",
+            "param": 4
+        },
+        {
+            "startTime": 37.99,
+            "type": "jump",
+            "param": 2
+        },
+        {
+            "startTime": 39.96,
+            "type": "box",
+            "param": 3
+        },
+        {
+            "startTime": 41.93,
+            "type": "box",
+            "param": 1
+        },
+        {
+            "startTime": 43.9,
+            "type": "box",
+            "param": 2
+        },
+        {
+            "startTime": 45.87,
+            "type": "box",
+            "param": 4
+        },
+        {
+            "startTime": 47.84,
+            "type": "jump",
+            "param": 3
+        },
+        {
+            "startTime": 49.81,
+            "type": "box",
+            "param": 1
+        },
+        {
+            "startTime": 50.81,
+            "type": "box",
+            "param": 1
         }
     ]
 }
